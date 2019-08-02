27bc00b131faacdf2127fb19491b99de1629dd7e
Eli Zane <1969761816@qq.com>
2019-08-02 10:54:37 +0800

    first commit This is a SMS.

---
+++ my-project-4/src/components/Customers.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="customers">
+    CUSTOMERS
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'customers',
+  data () {
+    return {
+      
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
